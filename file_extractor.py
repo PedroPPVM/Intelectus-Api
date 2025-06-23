09f22c45f84c8e3684cfc92d9e6932aee4e933da
Nickolas <nick@Nick.localdomain>
2025-06-23 15:41:08 -0300

    Web Scrapping dos PDFs: Desenhos Industriais, Marcas, Patentes e Programa de Computador\n\nOs pdfs são salvos em uma pasta Downloads que é criada ao executar o programa, caso não exista.

---
+++ file_extractor.py
@@ -0,0 +1,32 @@
+import os
+import re
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+
+
+BASE_URL = 'https://revistas.inpi.gov.br/rpi/'
+SAVE_DIR = 'downloads'
+
+os.makedirs(SAVE_DIR, exist_ok=True)
+
+response = requests.get(BASE_URL)
+soup = BeautifulSoup(response.content, 'html.parser')
+
+row = soup.select('table tr')[1:2][0]
+cells = row.find_all('a')
+
+links = []
+links.append(re.findall(r'"(.*?)"', str(cells[3]))[0])  # Desenhos industriais
+links.append(re.findall(r'"(.*?)"', str(cells[5]))[0])  # Marcas
+links.append(re.findall(r'"(.*?)"', str(cells[7]))[0])  # Patentes
+links.append(re.findall(r'"(.*?)"', str(cells[9]))[0])  # Programa de Computador
+
+for link in links:
+    file_name = os.path.join(SAVE_DIR, link.split('/')[-1])
+
+    print(f"Baixando: {link}")
+    file_response = requests.get(link)
+
+    with open(file_name, 'wb') as f:
+        f.write(file_response.content)
